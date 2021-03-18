e4343806ce4ad96313e1e7c6cceefa7dc54890d1
mharikmert <mhilmiarikmert@gmail.com>
2021-03-18 18:24:58 +0300

    mappings with react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MainPage from './pages/MainPage'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import React from 'react'
+class App extends React.Component {
+  render() {
+    return (
+        <>
+          <Router> {/* to avoid api calls every time*/}
+            <Route exact path = "/" component = {MainPage}/>
+            <Route path = "/login" component = {Login}/>
+            <Route path = "/register" component = {Register}/>
+          </Router> 
+        </>
+    )
+  }
 }
-
 export default App;
