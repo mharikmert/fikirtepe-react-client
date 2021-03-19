449d18fec0dc9bf37c685da92fd0a8e4c01f55ff
mharikmert <mhilmiarikmert@gmail.com>
2021-03-19 22:32:06 +0300

    registration screen sketch with bootstrap

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,56 @@
-import '../registration/vendor/mdi-font/css/material-design-iconic-font.min.css';
-import '../registration/vendor/font-awesome-4.7/css/font-awesome.min.css';
-import '../registration/vendor/select2/select2.min.css' 
-import '../registration/css/main.css';
-
 const Register = () => {
     return (
         <div>
-               registration page
+            <form className = 'form-horizontal'>
+                <h2 style = {headerStyle}> Fikirtepe Kayıt Formu </h2>
+                <Input type = 'text' placeholder = 'Ad'></Input>
+                <Input type = 'text' placeholder = 'Soyad'></Input>
+                <Input type = 'email' placeholder = 'Email'></Input>
+                <Input type = 'date' placeholder = 'Doğum Tarihi'></Input>
+                <Input type = 'phoneNumber' placeholder = 'Telefon Numarası'></Input>
+                <Input type = 'text' placeholder = 'Ad'></Input>
+                
+                <Button text = 'Kayıt Ol' type = 'submit' style = {buttonStyle}></Button> 
+            </form>
+            
         </div>
     )
 }
-
+const Input = ({type, placeholder}) => {
+    return (
+        <div className = 'form-group' role = 'form' style = {formStyle}>
+                <input className = 'form-control' type = {type} placeholder = {placeholder}></input>
+            {/* <div className = 'col-sm-9'>
+                <label className = 'col-sm-3 control-label' for = {type}> </label>
+            </div> */}
+        </div>
+    )
+    
+}
+const Button = ({text, type, style}) => {
+    return <button className = 'btn btn-primary btn-block' style = {style} type = {type} text = {text}> {text} </button>   
+}
+const formStyle = {
+    maxWidth: '530px',
+    padding: '15px',
+    margin: '0 auto',
+    borderRadius: '0.3em',
+    backgroundColor: '#f2f2f2'
+}
+const headerStyle = {
+    fontFamily: 'Open Sans sans-serif',
+    fontSize: '40px',
+    fontWeight: '600',
+    color: '#000000',
+    marginTop: '5%',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    letterSpacing: '4px',
+}
+const buttonStyle = {
+    width: '20%',
+    maxWidth: '530px',
+    margin: '25px auto',
+    backgroundColor: 'darkred'
+}
 export default Register
