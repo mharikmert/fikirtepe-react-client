acf9c3a503d6343720ae4ddfbfc974c3bda28962
mharikmert <mhilmiarikmert@gmail.com>
2021-03-19 22:30:34 +0300

    commented lines are deleted

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -10,12 +10,8 @@ class MainPage extends Component{
     return (
       <>
         <div className = 'top-line-container'>
-              {/* if you put anything between the opening and closing <img> tags (even just a single space),
-                gatsby appears to think it has child elements and will not compile properly.*/}
               <div className = "logo-container"> <img alt = 'cagdas logo' src = {cagdasLogo}></img></div>
               <div className = "title-container"> <h1>Fikirtepe Eğitime Destek Projesi </h1></div>
-              {/* <a href = "/register"> <button id = "register-buttonx" type = "button" className = "button"> KAYIT </button></a> */}
-              {/* <a href = "/login"> <button id = "login-buttonx" type = "button" className = "button"> GİRİŞ  </button> </a> */}
                 <MainPageButton text = 'KAYIT' onClick = { () => push('/register')}/>
                 <MainPageButton text = 'GİRİŞ' onClick = { () => push('/login')}/> 
         </div>
