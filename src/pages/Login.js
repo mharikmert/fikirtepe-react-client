434cadeffcb85c4297738fc571e765f11bc36334
mharikmert <mhilmiarikmert@gmail.com>
2021-03-25 18:08:43 +0300

    login processes are completed, structure is changed as functional component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,64 @@
 import '../css/style.css';
-import React from 'react'; 
 import axios from 'axios';
-class Login extends React.Component{
+import {useState} from 'react';
+
+const Login = props => {
     
-    state = {
-        id: null,
-        password: null
-    }
+    //useState hooks for user credentials 
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [warn, setWarn] = useState(false);
 
-    onChange = event  => {
-        const {name, value} = event.target; 
-        this.setState({
-            [name]: value
-        })
-        console.log(this.state);
-    }
-
-    onClick  = event => {
-        event.preventDefault();
-        const {id, password} = this.state; 
-        console.log(id, password)
-        const creds = {id, password};
-        console.log(creds)
-        axios.post('/api/login', creds);
+    const onClickLogin = () => {
+        const creds = {username, password};
+        //warning timeout
+        setTimeout ( () => setWarn(false), 1500);
+        setWarn(true);
+        //post credentials basic authentication 
+        axios.post('/api/auth',{},{auth:creds});
     }
     
-    render () {
+    const onKeyPress = event => {
+        if(event.key === 'Enter') onClickLogin();
+    }
+    //push -> redirection function
+    const {history} = props;
+    const {push} = history;
 
-        const {push} = this.props.history;
-        return (
-            <div id = 'LoginPage'>
-                <div id = "LoginTopLine">
-                    <div id = "LoginTitle">
-                        Öğrenci&nbsp;Bilgi&nbsp;Sistemi
-                    </div>
-
-                    <div className = "turn-back-btn-container">
-                        <button className = "turn-back-btn" onClick={() => push('/')}> Ana Sayfaya Dön </button>
-                    </div>
+    return (
+        <div id = 'LoginPage'>
+            <div id = "LoginTopLine">
+                <div id = "LoginTitle">
+                    Öğrenci&nbsp;Bilgi&nbsp;Sistemi
                 </div>
-                    {/* sample warning case */ }
-                    {/*<Warning message = 'Lütfen şifrenizi falan giriniz'/> */}
 
-
-                <div id = "LoginPanel">
-                    <div id= 'slogan'> EĞİTİM & DEĞİŞİM & GELİŞİM </div>
-                    <div id = "logo"></div>
-                        <div id = "form" >
-                            <Input id = 'id' name = 'id' placeholder = 'Kullanıcı Adı / TCKN' onChange = {this.onChange}></Input>
-                            <Input type = 'password' name = 'password' placeholder= 'Şifre' onChange = {this.onChange}></Input> 
-                            <button id = 'login-button' value = {'Giriş'} onClick = {this.onClick}></button>
-                            <PasswordDiv info = 'Şifremi unuttum'></PasswordDiv>
-                            <PasswordDiv info = 'Şifre Oluştur'></PasswordDiv>
-                        </div>
+                <div className = "turn-back-btn-container">
+                    <button className = "turn-back-btn" onClick={() => push('/')}> Ana Sayfaya Dön </button>
                 </div>
             </div>
-        )
-    }
+                {/* sample warning case */ }
+                {<Warning message = 'Lütfen şifrenizi falan giriniz' warn = {warn}/>}
+
+
+            <div id = "LoginPanel">
+                <div id= 'slogan'> EĞİTİM & DEĞİŞİM & GELİŞİM </div>
+                <div id = "logo"></div>
+                    <div id = "form" >
+                        <Input id = 'id' type = 'text' placeholder = 'Kullanıcı Adı / TCKN' onChange = {event => setUsername(event.target.value)} onKeyPress = {onKeyPress}></Input>
+                        <Input type = 'password' placeholder= 'Şifre' onChange = {event => setPassword(event.target.value)} onKeyPress = {onKeyPress}></Input> 
+                        <button className = 'input' id = 'login-button' onClick = {onClickLogin} onKeyPress = {onKeyPress}> GİRİŞ </button>
+                        <PasswordDiv info = 'Şifremi unuttum'></PasswordDiv>
+                        <PasswordDiv info = 'Şifre Oluştur'></PasswordDiv>
+                    </div>
+            </div>
+        </div>
+    )
 }
 
-const Warning = ({message}) => {
-    return <div className = 'warning-message'> {message} </div> 
+const Warning = ({message, warn}) => {
+    let display = warn ? 'block': 'none'
+
+    return <div className = 'warning-message' style = {{display: display}}> {message} </div> 
 }
 const PasswordDiv = ({info}) => {
     return <button id = 'password-div' className = 'input'> {info} </button>
@@ -68,7 +66,7 @@ const PasswordDiv = ({info}) => {
 const Input = (props) => {
     return (
         <input className = 'input' id = {props.id} name = {props.name} 
-        placeholder = {props.placeholder} value = {props.value} type = {props.type} onChange = {props.onChange}>
+        placeholder = {props.placeholder} value = {props.value} type = {props.type} onChange = {props.onChange} onKeyPress = {props.onKeyPress}>
         </input>
     )
 }
